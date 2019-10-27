85c9a839b0c1740798f18baf8fb321c562ab2957
Mickael <mickaelabelli@gmail.com>
2019-10-27 02:39:07 +0200

    window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include "tank.h"
+#include <raylib.h>
 
 int main(int argc, const char** argv) {
     
     //cout << "message" << endl;
+
+    InitWindow(500,500,"test");
+
     tank t(5);
     t.verif();
-    return 0;
+
+    
+    //fenetre raylib
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLUE);
+
+           
+
+        EndDrawing();
+    }
+    CloseWindow();
+    //return 0;
 }
